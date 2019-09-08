16776f8d877bc2d95f83d6d78aa658f5c45b61ce
Pixel-0 <tiny0pixel@gmail.com>
2019-09-08 18:01:26 +0300

    create profile model

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+   '''
+   Holds user's profile data.
+   '''
+
+   user = models.OneToOneField(User, on_delete=models.CASCADE)
+   avatar = models.ImageField(upload_to='avatars/',null=True,default='default.jpg')
+   bio = models.TextField(max_length=140,null=True)
+
+   def __str__(self):
+      return self.user.username
