6e8b61587cafa2aee5ad5864aab5a31e8f422727
Pixel-0 <tiny0pixel@gmail.com>
2019-09-08 18:20:27 +0300

    add url patterns for app

--- awards/urls.py
+++ awards/urls.py
@@ -2,3 +2,18 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from .forms import LoginForms
+
+urlpatterns = [
+   path('', views.home, name='home'),
+   path('profile/',views.profile,name='profile'),
+   path('register/',views.register,name='register'),
+   path('login/', auth_views.LoginView.as_view(authentication_form=LoginForm,extra_context={'title': 'Login'}), name='login'),
+   path('logout/', views.logout_view, name='logout'),
+   path('profile/update',views.edit_profile,name='update_profile'),
+   path('project/new',views.new_project,name='new_project'),
+   path('project/view/<int:id>',views.project_view,name='project_view'),
+   path('search/',views.search,name="search"),
+   # Extra views
+   path('api/profiles/',views.ProfileList.as_view()),
+   path('api/projects/',views.ProjectList.as_view())
+]
